e32eb31c494b1542f90975b972227c447ce7de06
ProwlingLynx <tamezjavier@gmail.com>
2022-09-27 13:27:08 -0500

    Updated file name. Debugged function.

---
+++ hi-low-guessing-game/main.py
@@ -0,0 +1,4 @@
+from utils import get_user_guess, create_game_object
+
+print(create_game_object(False))
+print(get_user_guess())
--- hi-low-guessing-game/utils.py
+++ hi-low-guessing-game/utils.py
@@ -2,12 +2,21 @@ from random import randint
 def generate_number():
     """Returns an int [1, 100]"""
     return randint(1, 100)
+
 def get_user_guess():
     """Gets user input and outputs an integer. Checks against potential bad inputs."""
-    user_input = int(input("Guess a number between and including 1 - 100. Whole numbers only please.\n"))
-    while not isinstance(user_input, int) and user_input > 0 and user_input <= 100:
-        user_input = int(input("Incorrect input. Please enter a number from 1 - 100.\n"))
+    try:
+        user_input = int(input("Guess a number between and including 1 - 100. Whole numbers only please.\n"))
+    except:
+        user_input = -1
+    while not isinstance(user_input, int) or user_input < 1 or user_input > 100:
+        try:
+            user_input = int(input("Incorrect input. Please enter a number from 1 - 100.\n"))
+        except:
+            print("Non number input detected!")
+            user_input = -1
     return user_input
+
 def create_game_object(is_hard):
     '''
     Params: is_hard - true for 5 guess game. false for 10 guess game.
