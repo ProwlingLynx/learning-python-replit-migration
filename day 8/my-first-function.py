442762e46bc29ce1febb252ae0c0f53a0ea88438
Javier Tamez <tamezjavier@gmail.com>
2022-12-29 21:54:24 -0600

    migrate day 8 project.

---
+++ day 8/my-first-function.py
@@ -0,0 +1,13 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+
+def greet(a, b):
+  print(f"eh, hello {a}")
+  print(f"my, {a} good of you to join!")
+  print(f"Go over to {b} {a}... They ave a lot to talk about")
+greet("Kyle", "Chickville")
+
+greet(b="ClarkVile", a  = "suuu")
